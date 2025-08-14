2c202a94a4c504b0b3d32a72339c71773aabe2d2
moprei21 <moritz.preisig48@gmail.com>
2025-08-14 09:11:30 +0200

    script to run mauve experiments

---
+++ run_mauve.sh
@@ -0,0 +1,21 @@
+python3 text_similarity.py fewshot zürich > results/results_zürich_fewshot.txt
+python3 text_similarity.py fewshot basel > results/results_basel_fewshot.txt
+python3 text_similarity.py fewshot luzern > results/results_luzern_fewshot.txt
+python3 text_similarity.py fewshot bern > results/results_bern_fewshot.txt
+
+
+python3 text_similarity.py fewshot_conversational zürich > results/results_zürich_fewshot_conversational.txt
+python3 text_similarity.py fewshot_conversational basel > results/results_basel_fewshot_conversational.txt
+python3 text_similarity.py fewshot_conversational luzern > results/results_luzern_fewshotconversational.txt
+python3 text_similarity.py fewshot_conversational bern > results/results_bern_fewshotconversational.txt
+
+
+python3 text_similarity.py zero_shot zürich > results/results_zürich_zero_shot.txt
+python3 text_similarity.py zero_shot basel > results/results_basel_zero_shot.txt
+python3 text_similarity.py zero_shot luzern > results/results_luzern_zero_shot.txt
+python3 text_similarity.py zero_shot bern > results/results_bern_zero_shot.txt
+
+python3 text_similarity.py finetuned_zeroshot zürich > results/results_zürich_finetuned_zeroshot.txt
+python3 text_similarity.py finetuned_zeroshot basel > results/results_basel_finetuned_zeroshottxt
+python3 text_similarity.py finetuned_zeroshot luzern > results/results_luzern_finetuned_zeroshot.txt
+python3 text_similarity.py finetuned_zeroshot bern > results/results_bern_finetuned_zeroshot.txt
